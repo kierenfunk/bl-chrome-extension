b8fd3d8df452e632794108e245d7b9618da34999
Kieren Funk <kierenfunk@gmail.com>
2021-11-10 12:51:43 +0100

    firebaseui login popup

--- src/views/popup.tsx
+++ src/views/popup.tsx
@@ -1,18 +1,17 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import SignInScreen from '../components/SignIn';
 
-const Popup = () => {
-  return <div>Hello, world!</div>;
-}
+const Popup = () => <SignInScreen/>;
 
-chrome.tabs.query({ active: true, currentWindow: true }, tab => {
+chrome.tabs.query({ active: true, currentWindow: true }, () => {
   ReactDOM.render(<Popup />, document.getElementById('popup'));
 });
 
-/*export default function Popup() {
+/* export default function Popup() {
   useEffect(() => {
     // Example of how to send a message to eventPage.ts.
     chrome.runtime.sendMessage({ popupMounted: true });
   }, []);
 
-}*/
+} */
